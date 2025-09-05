369f0be14142bfb07c7c4a667b4a4a8a05d671c7
alien_0119 <wangtianyu43@huawei.com>
2025-09-05 11:41:22 +0800

    add encodec

--- mindone/transformers/models/encodec/modeling_encodec.py
+++ mindone/transformers/models/encodec/modeling_encodec.py
@@ -366,7 +366,9 @@ class EncodecEuclideanCodebook(nn.Cell):
         embed = self.embed.t()
         scaled_states = hidden_states.pow(2).sum(1, keepdim=True)
         dist = -(scaled_states - 2 * hidden_states @ embed + embed.pow(2).sum(0, keepdim=True))
-        embed_ind = dist.max(dim=-1).indices
+        # FIXME: tensor.max of mindspore and torch has different output type
+        # embed_ind = dist.max(dim=-1).indices
+        embed_ind = dist.max(dim=-1)[1]
         return embed_ind
 
     def encode(self, hidden_states):
