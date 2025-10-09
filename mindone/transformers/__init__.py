8e1d1910d7cd02e98dfb16c60d8b451653b0ccd3
alien_0119 <wangtianyu43@huawei.com>
2025-10-09 15:10:12 +0800

    add doge

--- mindone/transformers/__init__.py
+++ mindone/transformers/__init__.py
@@ -303,6 +303,7 @@ from .models.deberta_v2 import (
 )
 from .models.depth_anything import DepthAnythingForDepthEstimation, DepthAnythingPreTrainedModel
 from .models.dinov2 import Dinov2Backbone, Dinov2ForImageClassification, Dinov2Model, Dinov2PreTrainedModel
+from .models.doge import DogeForCausalLM, DogeForSequenceClassification, DogeModel, DogePreTrainedModel
 from .models.dpt import DPTForDepthEstimation, DPTImageProcessor, DPTModel, DPTPreTrainedModel
 from .models.fuyu import FuyuForCausalLM, FuyuPreTrainedModel
 from .models.gemma import (
